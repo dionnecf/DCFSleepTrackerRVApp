e7bba7414ca4ccf8a69ea75be4a99a7a85a70e3f
Dionne Condor-Farrell <info@dcfhosting.co.uk>
2021-01-10 16:23:58 +0000

    In the previous commit we changed ScrollView to a Recyc

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -23,7 +23,6 @@ import com.example.android.trackmysleepquality.database.SleepNight
 
 class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
     private var data = listOf<SleepNight>()
-
     override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
